9456872e963db2e86804d34b7fd899dc6f67093e
Ganesh Karamala <ganeshkaramala@gmail.com>
2018-01-04 23:31:42 +0530

    angular seo with prerender.io
    Signed-off-by: Ganesh Karamala <ganeshkaramala@gmail.com>

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/app/components',express.static(__dirname + '/app/components'));
 app.use('/assets', express.static(__dirname + '/app/assets'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/weekendcinema.min.js', express.static(__dirname + '/app-all.js'));
+app.use(require('prerender-node').set('prerenderToken', 'Ob2gHB2BmFQbhcj7OMtd'));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
